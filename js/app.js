409267804db441d893093300569ec33cf66347d1
Gabriel Diniz <contato@gabrieldiniz.com>
2014-07-07 16:52:05 -0300

    que haja luzgit add .

---
+++ js/app.js
@@ -0,0 +1,80 @@
+$$.json(
+	'http://financeiro.doctum.edu.br/notasMobile.php',
+	{aluno:"130600012"},
+	function(dados) {
+		for(var disciplina in dados){
+			/**
+			* Montando Menu-lateral
+			**/
+			$$('#aside-itens').append('\
+				<li data-view-article="'+dados[disciplina].abrev+'" data-icon="list"> \
+					<strong>'+disciplina+'</strong> \
+				</li>'
+			);
+
+			/**
+			* Montando página inicial
+			**/
+			if (parseFloat(dados[disciplina].nota)<70) {
+				cor = 'red';
+			} else{
+				cor = 'blue';
+			}
+			$$('#home-itens').append('\
+				<li>\
+					<div class="on-right grade "> <span class="'+cor+'">'+dados[disciplina].nota+'</span></div>\
+					<strong>'+disciplina+'</strong>\
+					<!--<small>Nota: <span class="'+cor+'">'+dados[disciplina].nota+'</span></small>-->\
+				</li>'
+			);
+
+			/**
+			* Montando paginas das disciplinas
+			**/
+
+			$$('#main').append('\
+				<article class="list indented" id="'+dados[disciplina].abrev+'">\
+					<li class="">Agendamentos</li>\
+				</article>'
+			);
+
+			/**
+			* Montando os agendamentos das disciplinas
+			**/
+			i=0;
+			agendamentos='';
+			for(var etapa in dados[disciplina].etapas){
+				
+				/**
+				* Montado etapas
+				**/
+				agendamentos=agendamentos+'\
+					<li class="theme">\
+						<div class="on-right grade "> <span class="'+cor+'">'+dados[disciplina].etapas[etapa].nota+'</span></div>\
+						'+etapa+'\
+					</li>\
+					<ul id="#agenda-'+dados[disciplina].abrev+'-'+i+'">';
+
+				for (var agendamento in dados[disciplina].etapas[etapa].agenda){
+					agendamentos = agendamentos + '\
+						<li>\
+							<div class="on-right grade "> <span class="'+cor+'">'+dados[disciplina].etapas[etapa].agenda[agendamento].nota+'</span></div>\
+							<strong>'+agendamento+'</strong>\
+							<small>Data: '+dados[disciplina].etapas[etapa].agenda[agendamento].data+'</small>\
+							<small>Valor: '+dados[disciplina].etapas[etapa].agenda[agendamento].valor+'</small>\
+						</li>'
+					;
+				}
+
+				agendamentos=agendamentos+'\
+					</ul>'
+				
+				
+				
+				i++;
+			}
+			$$('#'+dados[disciplina].abrev).append(agendamentos);
+		}
+	}
+);
+
